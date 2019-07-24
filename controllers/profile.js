38eb3d3a6850aeffe4a381e9132ac53e659296d7
kcgreenn <kcgreenn@gmail.com>
2019-07-24 10:57:12 -0400

    Add post profile protected route and controller

--- controllers/profile.js
+++ controllers/profile.js
@@ -16,3 +16,97 @@ exports.getProfile = (req, res) => {
     })
     .catch(error => res.status(404).json({ error }));
 };
+
+exports.postProfile = (req, res) => {
+  const errors = {};
+  // Pull data from Fields
+  let profileFields = ({
+    handle,
+    occupation,
+    bio,
+    location,
+    status,
+    interests,
+    expreience,
+    education,
+    social = {},
+    instagram,
+    linkedin,
+    youtube
+  } = req.body);
+  console.log(profileFields);
+  //   Split interests into array
+  profileFields.interests =
+    typeof profileFields.interests !== undefined
+      ? profileFields.interests.split("")
+      : null;
+
+  //   Social Links
+  profileFields.social = {
+    youtube,
+    instagram,
+    linkedin
+  };
+
+  Profile.findOne({ user: req.user.id }).then(profile => {
+    if (!profile) {
+      // If profile does not exist
+      // Check if handle exists
+      Profile.findOne({ handle: profileFields.handle })
+        .then(profile => {
+          errors.handle = "That handle is already in use.";
+          res.status(400).json(errors);
+        })
+        .catch(error => {
+          errors.error = error;
+          res.status(404).json(errors);
+        });
+      // Create Profile
+      new Profile(profileFields)
+        .save()
+        .then(profile => res.json(profile))
+        .catch(error =>
+          res.status(404).json({ error: "Could not create profile" })
+        );
+    }
+  });
+};
+
+exports.putProfile = (req, res) => {
+  const errors = {};
+  // Pull data from fields
+  const { user } = req.user.id;
+  let profileFields = ({
+    handle,
+    occupation,
+    bio,
+    location,
+    status,
+    interests,
+    expreience,
+    education,
+    social = {},
+    instagram,
+    linkedin,
+    youtube
+  } = req.body);
+  //   Split interests into array
+  profileFields.interests =
+    typeof interests !== undefined ? interests.split("") : null;
+
+  //   Social Links
+  profileFields.social = {
+    youtube,
+    instagram,
+    linkedin
+  };
+  Profile.findOne({ user }).then(profile => {
+    if (profile) {
+      Profile.findOneAndUpdate({ user }, { $set: profileFields }, { new: true })
+        .then(profile => res.json(profile))
+        .catch(error =>
+          res.status(404).json({ error: "Could not update profile" })
+        );
+    }
+  });
+};
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,18 +2,32 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 
-const { getProfile } = require("../../controllers/profile");
+const {
+  getProfile,
+  postProfile,
+  putProfile
+} = require("../../controllers/profile");
 
-// @route   Get api/profile/test
+// @route   GET api/profile/test
 // @desc    Tests Profile route
 // @access  Public
 router.get("/test", (req, res) => {
   res.send({ message: "Profile Works" });
 });
 
-// @route   Get api/profile
+// @route   GET api/profile
 // @desc    Returns profile of current user
 // @access  Private
 router.get("/", passport.authenticate("jwt", { session: false }), getProfile);
+
+// @route   POST api/profile
+// @desc    Create user's profile
+// @access  Private
+router.post("/", passport.authenticate("jwt", { session: false }), postProfile);
+
+// @route   PUT api/profile
+// @desc    Edit user's profile
+// @access  Private
+router.put("/", passport.authenticate("jwt", { session: false }), putProfile);
 
 module.exports = router;
