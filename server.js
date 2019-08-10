6b5f15ce3886cd071250d4812ff9b569bbcd1d16
kcgreenn <kcgreenn@gmail.com>
2019-08-10 16:39:24 -0400

    prepare for deployment

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 require("dotenv").config();
+const path = require("path");
 
 const users = require("./routes/api/users");
 const posts = require("./routes/api/posts");
@@ -37,6 +38,16 @@ require("./config/passport.js")(passport);
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+// Sreve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path / resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
