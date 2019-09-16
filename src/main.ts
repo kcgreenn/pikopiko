f800f1c55dfd6c61501cbf63cde9def0fc85af4c
KC Green <kcgreenn@gmail.com>
2019-09-15 21:25:36 -0400

    Deploy

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 
-const port = parseInt(process.env.PORT) | 4000;
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
+  const port = parseInt(process.env.PORT) || 4000;
   Logger.log(`Server is running on port: ${port}`);
   await app.listen(port);
 }
