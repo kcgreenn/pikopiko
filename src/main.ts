7d949e61dab697b6cd107a3f073399e53cd78444
KC Green <kcgreenn@gmail.com>
2019-09-16 11:55:25 -0400

    Prepared for deployment

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
